913058d6393df28b5c6934e8e7e1fb4a0aa632f2
crystalcast1es <avdeevveip@gmail.com>
2026-09-07 01:34:15 +0200

    done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfTimes) {
+    let repeatedString = '';
+    if(numberOfTimes >= 0){
+        for(let i = 0; i < numberOfTimes; i++){
+            repeatedString += word;
+        } 
+        return repeatedString
+    }
+    else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
