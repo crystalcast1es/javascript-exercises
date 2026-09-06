e68722b10fd9b5add1d20460f35bee4c3d7b89cb
crystalcast1es <avdeevveip@gmail.com>
2026-09-07 01:46:54 +0200

    done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let temp = string.split('');
+    let reversedTemp = [];
+    for (let index = 0; index < temp.length; index++) {
+        reversedTemp.push(temp[temp.length - index - 1]);
+    }
+    return reversedTemp.join('');
 };
 
 // Do not edit below this line
