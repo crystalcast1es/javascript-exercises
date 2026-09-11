8591674e7811806e3858a0915c8484a1c6780771
crystalcast1es <avdeevveip@gmail.com>
2026-09-12 00:56:51 +0200

    done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numberA, numberB) {
+    let sumOfNumbers = 0;
+    if(numberA <= 0 || numberB <= 0 || typeof(numberA) !== "number" || 
+    typeof(numberB) !== "number" || !Number.isInteger(numberA) || !Number.isInteger(numberB)) {
+        return 'ERROR'
+    }
+    else if(numberA < numberB) {
+        for(numberA; numberA <= numberB; numberA++) {
+            sumOfNumbers += numberA;
+        }
+        return sumOfNumbers;
+    }
+    else {
+        for (numberB; numberB <= numberA; numberB++) {
+            sumOfNumbers += numberB;
+        }
+        return sumOfNumbers;
+    }
 };
 
 // Do not edit below this line
