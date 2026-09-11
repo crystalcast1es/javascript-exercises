b36f7df690568b0b658c479fcf331a65b220b69d
crystalcast1es <avdeevveip@gmail.com>
2026-09-11 02:11:48 +0200

    done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    let difference = array.filter((x) => !theArgs.includes(x));
+    return difference;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
