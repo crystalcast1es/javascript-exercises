d44c2fcbce3d4b693b6a647fe4e96e1cdf07b658
crystalcast1es <avdeevveip@gmail.com>
2026-09-12 02:26:51 +0200

    done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFahrenheit) {
+  return +((degreeFahrenheit-32)*(5/9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelsius) {
+  return +((9/5)*degreeCelsius + 32).toFixed(1);
 };
 
 // Do not edit below this line
